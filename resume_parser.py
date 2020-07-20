50f67cdcc0f01a08bf2fccb4ca812153a121d8e2
Liza Pressman <lizapressman@Lizas-MacBook-Pro.local>
2020-07-20 11:59:16 -0400

    create html file

--- resume_parser.py
+++ resume_parser.py
@@ -1,3 +1,27 @@
 from pyresparser import ResumeParser
-data = ResumeParser('/path/to/resume/file').get_extracted_data()
 
+data = ResumeParser('/Users/lizapressman/Documents/ResumeToWebsite/resume.pdf').get_extracted_data()
+
+print(data)
+
+for key in data:
+    print(key)
+    print(data.get(key))
+
+# ask user to verify and change information on front end side
+# update/fix data on submit from user
+
+website = open('website.html', 'w')
+
+message = f"""
+<html>
+    <head></head>
+    <body>
+        <h1>Name: {data.get("name")}</h1>
+        <p>Email: {data.get("email")}</p>
+    </body>
+</html>
+"""
+
+website.write(message)
+website.close()
